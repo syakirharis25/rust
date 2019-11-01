c4c4711ecb5e8e6b0c37890817e138944d7a58ef
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2019-11-01 09:55:08 +0800

    first attempt to upload my rust files before friday prayer

---
+++ comments/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // The macro below prints 'hello world' to the console
+    // this is an example
+
+    /*
+    here
+    is
+    a
+    comment
+    */
+
+    println!("Hello, world!");
+}
---
+++ variables/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let x: u64 = 45; // u64 -- u means unsigned, not a negative number, 64 bit
+    let f: f32 = 6.7; // f32 -- f means floating point number, 32 bit
+    let b: bool = false; // b means boolean
+}
